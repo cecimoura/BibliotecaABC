3bf6c29349009d5e1a94e029c94b8d2152cd7182
cecimoura <cecimoura91@gmail.com>
2025-08-03 00:14:16 -0300

    LivroService atualizado com update, save e delete

--- src/main/java/com/biblioteca/biblioteca_ABC/service/LivroService.java
+++ src/main/java/com/biblioteca/biblioteca_ABC/service/LivroService.java
@@ -23,11 +23,32 @@ public class LivroService {
         return null;
     }
 
-    //save, update, delete
+    public Livro save(Livro livro){
+        livros.add(livro);
+        return livro;
+    }
 
+    public Livro update(int id, Livro livroUpdate) {
+        Livro livro = findById(id);
 
+        if (livro != null) {
+            livro.setIssn(livroUpdate.getIssn());
+            livro.setTitulo(livroUpdate.getTitulo());
+            livro.setSinopse(livroUpdate.getSinopse());
+            livro.setAno(livroUpdate.getAno());
+            livro.setNumeroPaginas(livroUpdate.getNumeroPaginas());
+        }
+        return livro;
+    }
 
-
-
+    public void delete(int id){
+        for(int i = 0; i < livros.size(); i++){
+            if(livros.get(i).getId() == id){
+                livros.remove(i);
+                break;
+            }
+        }
+    }
+    
 }
 
